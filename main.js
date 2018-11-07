3cdf2015f1831621cbbc6c0ec5b0622579d8335a
Andres Outon <outon.x@gmail.com>
2018-11-07 02:01:36 -0600

    added functionality for modal and to load iframe

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-lol = function() {
-  group = Array.from(document.querySelectorAll(iframe));
+const items = Array.from(document.querySelectorAll("img"));
+const frame = document.querySelector("iframe");
+const modal = document.querySelector(".modal");
+const button = document.querySelector(".menu-btn");
+
+const hideModal = () => {
+  modal.classList.add("hidden");
+  frame.src = "";
 };
+
+items.forEach(item =>
+  item.addEventListener("click", e => {
+    const link = e.target.attributes[1].value;
+    frame.src = link;
+    modal.classList.remove("hidden");
+  })
+);
+
+button.addEventListener("click", hideModal);
+modal.addEventListener("click", hideModal);
